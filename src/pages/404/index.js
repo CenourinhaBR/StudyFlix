3a64c5ddb39993dcab912ce79389f4fdb9de425b
CenourinhaBR <jogadorbr602@gmail.com>
2020-07-28 21:30:37 -0300

    Adicionado Desafio da Pagina404

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import '../../global.css';
 
+import './style.css'
+
+import Img404 from '../../assets/Img/Error404.png'
+import Logo from '../../assets/Img/Logo.png'
+import { Link } from 'react-router-dom';
 
 function Error() {
   return (
     <div>
-        <h1>Error 404</h1>
-    
+        <img src={Logo} alt="Imagem de Erro" className="Logo_Error"/>
+        <h1>Você se perdeu do seu Caminho?</h1>
+        <h2>Infeslimente não encontrei o seu caminho. Que tal voltar?<Link className="Link" to="/"> Clique aqui</Link>  </h2>
+        <img src={Img404} alt="Imagem de Erro"/>
     </div>
   );
 }
